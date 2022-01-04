e129c8896678f22027009ebfb29a3d8d73a0fc2a
Supplepentan <64350376+supplepentan@users.noreply.github.com>
2022-01-05 06:09:43 +0900

    Delete ffmpeg.py

--- ffmpeg.py
+++
@@ -1,7 +0,0 @@
-import subprocess
-
-#subprocess.run(["ffmpeg", "-i", "input/test.webm", "-vcodec", "copy", "input/test_trans.mp4"])
-
-#subprocess.run(["ffmpeg", "-i", "input/test1.mp4", "-vcodec", "mjpeg", "input/image_%03d.jpg"])
-
-subprocess.run(["ffmpeg", "-r", "30", "-i", "cartoonized_images/%06d.jpg", "-vcodec", "libx264", "-r", "60", "cartoonized_images/out.mp4"])
